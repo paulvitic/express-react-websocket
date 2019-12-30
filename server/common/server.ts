9cf2956b6a6834a4d9f5607a0607b1544e1a7518
paulvitic <git1212Paul>
2019-12-30 17:29:53 +0100

    move web socket server out

--- server/common/server.ts
+++ server/common/server.ts
@@ -2,15 +2,13 @@ import express, { Application } from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import http from 'http';
-import * as WebSocket from 'ws';
 import os from 'os';
 import session from 'express-session';
 import log from './logger';
-
-
 import installValidator from './swagger';
 import viewCounter from "../api/middlewares/view.counter";
 import sessionConfig from "./sessionConfig";
+import WebSocketServer from "./webSocketServer";
 
 const app = express();
 const exit = process.exit;
@@ -44,47 +42,9 @@ export default class ExpressServer {
     };
 
     installValidator(app, this.routes).then(() => {
-      // http.createServer(app).listen(port, welcome(port));
-
-      ////////////////////////////////////////
-      // add web socket listener /////////////
       const server = http.createServer(app);
-
-      const wss = new WebSocket.Server({ server });
-      wss.on('connection', (ws: WebSocket) => {
-
-        //connection is up, let's add a simple simple event
-        ws.on('message', (message: string) => {
-
-          //log the received message and send it back to the client
-          log.info(`received: ${message}`);
-
-          const broadcastRegex = /^broadcast\:/;
-
-          if (broadcastRegex.test(message)) {
-            message = message.replace(broadcastRegex, '');
-
-            //send back the message to the other clients
-            wss.clients
-                .forEach(client => {
-                  if (client != ws) {
-                    client.send(`Hello, broadcast message -> ${message}`);
-                  }
-                });
-
-          } else {
-            log.info(`received: ${message}`);
-            ws.send(`Hello, you sent -> ${message}`);
-          }
-        });
-
-        //send immediately a feedback to the incoming connection
-        ws.send('Hi there, I am a WebSocket server');
-      });
-
+      new WebSocketServer(server);
       server.listen(port, welcome(port));
-      // TODO add heartbeat, see: https://gist.github.com/thiagof/aba7791ef9504c1184769ce401f478dc
-      ////////////////////////////////////
 
     }).catch(e => {
       log.error(`Error while calling Server listen : ${e}`);
